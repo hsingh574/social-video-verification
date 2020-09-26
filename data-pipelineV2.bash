a24603f42e321fd019a588808822e2cf97cee5ea
hsingh574 <hsingh574@gmail.com>
2020-09-26 16:56:05 -0700

    fixing bash script

--- data-pipelineV2.bash
+++ data-pipelineV2.bash
@@ -53,7 +53,7 @@ do
    source wav2lip/bin/activate
    
    
-   python3 inference.py --checkpoint_path "${WAV2LIP_CHECKPOINT}" --face "${VIDEO_LOCATION}/cam${i}-lipgan/cam${i}-first-4k.mp4" --fps 29.97 --audio "audio/${AUDIO_FILENAME}.wav"
+   python3 inference.py --checkpoint_path "${WAV2LIP_CHECKPOINT}" --face "${VIDEO_LOCATION}/cam${i}-lipgan/cam${i}-first-4k.mp4" --audio "audio/${AUDIO_FILENAME}.wav"
    mv results/result_voice.mp4 "${DEEPFAKE_LOCATION}/cam${i}-wav2lip/cam${i}-wav2lip.mp4"
    
    deactivate
