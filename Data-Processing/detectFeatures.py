702a5a7eee4b02b5da5ee2b5174228f47f6dc709
hsingh574 <hsingh574@gmail.com>
2020-09-27 16:17:31 -0700

    testing landmarks

--- Data-Processing/detectFeatures.py
+++ Data-Processing/detectFeatures.py
@@ -17,22 +17,27 @@ boundingFile = open(bounding, "r")
 
 numImg = len([name for name in os.listdir(inDir) if os.path.isfile(os.path.join(inDir, name))])
 for f in range(1,numImg+1):
-	filename = inDir + "frames" + '{0:04d}'.format(f) + ".jpg"
-	
-	box = boundingFile.readline().split(',')
-	boxInt = [[int(box[0]),int(box[1]), int(box[2]), int(box[3])]]
-	 
-	print("Processing file: {}".format(f))
-	img = io.imread(filename)
-	pred = fa.get_landmarks_from_image(img,detected_faces=boxInt)
-	pred = np.array(pred)
-	
-	if (pred.size == 1):
-		print("No face detected")
-	else:
-		curData = np.reshape(pred,(68,-1))	
-	
-	np.save(outDir + 'landmarks2D-' + '{0:04d}'.format(f) + '.npy',curData)
+    filename = inDir + "frames" + '{0:04d}'.format(f) + ".jpg"
+    
+    box = boundingFile.readline().split(',')
+    
+    try:
+        boxInt = [[int(box[0]),int(box[1]), int(box[2]), int(box[3])]]
+    except ValueError as e:
+        print(f'box0: {box[0]}, box1: {box[1]}, box2: {box[2]}, box3: {box[3]}')
+        raise ValueError(e)
+     
+    print("Processing file: {}".format(f))
+    img = io.imread(filename)
+    pred = fa.get_landmarks_from_image(img,detected_faces=boxInt)
+    pred = np.array(pred)
+    
+    if (pred.size == 1):
+        print("No face detected")
+    else:
+        curData = np.reshape(pred,(68,-1))    
+    
+    np.save(os.path.join(outDir, 'landmarks2D-' + '{0:04d}'.format(f) + '.npy'),curData)
 
 
 boundingFile.close()
