c5be7bf8a37d7f98d00c088ecebf2da145e00026
hsingh574 <hsingh574@gmail.com>
2020-10-06 10:32:22 -0700

    preparing all ID generation

--- data-pipelineV2.bash
+++ data-pipelineV2.bash
@@ -109,7 +109,7 @@ if [[ ${SINGLE_ID} =~ ID([1-9]|[1-9]{1}[0-9]{1})$ ]]; then
 else
 
 # loop over all ID directories in base directory
-    for dir in ${DEEPFAKE_LOCATION_BASE}/*/ ;
+    for dir in ${VIDEO_LOCATION_BASE}/*/ ;
     do
         echo ${dir}
         
