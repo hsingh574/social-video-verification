5c77fa4b8df548925feb96c5f0670c89bb4f1002
APikielny <apikielny@gmail.com>
2020-12-11 16:29:00 -0800

    debugging

--- Experiments/relight_window_accuracy_experiment.m
+++ Experiments/relight_window_accuracy_experiment.m
@@ -18,7 +18,7 @@ for p=1:length(people)
     
     % eg: fake3 indicates that the fake in the struct is the lipgan output  
     % of camera 3
-    data4 = load(['DataSHCoords/fake4-ID' person '.mat']);
+    data4 = load(['DataSHCoords/fake4-ID' person '-300frames.mat']);
     
     fullLen = length(data4.cam1);
     
