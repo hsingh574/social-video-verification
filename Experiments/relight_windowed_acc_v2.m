c4cfabb4c12814b821ea7b1615d7812371687e5a
APikielny <apikielny@gmail.com>
2020-12-13 16:17:01 -0800

    plotting

--- Experiments/relight_windowed_acc_v2.m
+++ Experiments/relight_windowed_acc_v2.m
@@ -40,8 +40,11 @@ for p=1:length(people)
     %fake4 = randi([0, 1], [1500,9]);
     %fake2 = randi([0, 1], [1500,9]);
 
-    differences = [sum(mean(cam1 - fake4)), sum(mean(cam2 - fake4))];
+    differences = [sum(mean(cam1 - fake4)), sum(mean(cam2 - fake4)), sum(mean(cam3 - fake4)), sum(mean(cam4 - fake4)), sum(mean(cam5 - fake4)), sum(mean(cam6 - fake4))];
     plot(differences);
+    title('Cam 1-6 vs. Fake4');
+    xlabel('Camera');
+    ylabel('Sum of mean difference of SH Coords');
     
     % calculate L2 between real/fake mouth landmarks for all frames
     baseline = vecnorm(cam4 - fake4, 2, 2);
