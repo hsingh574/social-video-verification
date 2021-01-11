a65d99bbdf558e2be09810438eac76190e6051d3
hsingh574 <hsingh574@gmail.com>
2021-01-11 10:42:32 -0800

    fixing dataset processing

--- Data-Processing/deeperForensicsProcess.py
+++ Data-Processing/deeperForensicsProcess.py
@@ -8,6 +8,7 @@ Created on Mon Jan 11 09:22:02 2021
 
 import os
 import argparse
+import re
 from shutil import copy2
 
 def parse_args():
@@ -29,6 +30,8 @@ def main():
     
     id_count = 1
     
+    r = re.compile("camera*")
+    
     #iterate over dataset parts
     for i in range(args.num_parts):
         source_video_dir = os.path.join(args.data_dir, 'source_videos' + str(i))
@@ -42,18 +45,23 @@ def main():
                 type_name_video_dir = os.path.join(name_video_dir, dir_type)
                 
                 #If in BlendShape situation
-                if 'camera_down' in list(os.listdir(type_name_video_dir)):
+                if len(list(filter(r.match, list(os.listdir(type_name_video_dir))))) > 0:
+                    if len(list(os.listdir(type_name_video_dir))) < 5:
+                        continue
                     ID_dir = os.path.join(args.data_dir, 'ID' + str(id_count))
                     os.makedirs(ID_dir)
                     id_count += 1
                     for cam_num, angle in enumerate(os.listdir(type_name_video_dir)):
                         temp = os.path.join(type_name_video_dir, angle)
                         file = os.path.join(temp, list(os.listdir(temp))[0])
-                        copy2(file, os.path.join(ID_dir, 'camera' + str(cam_num)+'.mp4'))
+                        os.rename(file, os.path.join(ID_dir, 'camera' + str(cam_num)+'.mp4'))
                 else:
                     #Iterate over emotions in lighting conditions
                     for emotion in os.listdir(type_name_video_dir):
                         emotion_video_dir = os.path.join(type_name_video_dir, emotion)
+                        if len(list(os.listdir(emotion_video_dir))) < 5:
+                            continue
+                        
                         ID_dir = os.path.join(args.data_dir, 'ID' + str(id_count))
                         os.makedirs(ID_dir)
                         id_count += 1
@@ -62,7 +70,7 @@ def main():
                         for cam_num, angle in enumerate(os.listdir(emotion_video_dir)):
                             temp = os.path.join(emotion_video_dir, angle)
                             file = os.path.join(temp, list(os.listdir(temp))[0])
-                            copy2(file, os.path.join(ID_dir, 'camera' + str(cam_num)+'.mp4'))
+                            os.rename(file, os.path.join(ID_dir, 'camera' + str(cam_num)+'.mp4'))
           
                 
             
