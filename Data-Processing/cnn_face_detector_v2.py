b41aea7a396e96fbc7c03ccf2089c2b3776844fb
hsingh574 <hsingh574@gmail.com>
2021-01-17 16:46:14 -0800

    face detector changing

--- Data-Processing/cnn_face_detector_v2.py
+++ Data-Processing/cnn_face_detector_v2.py
@@ -16,11 +16,18 @@ save_path = sys.argv[3]
 cnn_face_detector = dlib.cnn_face_detection_model_v1(face_detector_path)
 
 
+
 print('Reading video frames...')
 
+numImg = len([name for name in os.listdir(frames_path) if 
+                  os.path.isfile(os.path.join(frames_path, name))])
+
+
 with open(save_path, 'w+') as out:
-    for f in os.listdir(frames_path):        
-        img = dlib.load_rgb_image(os.path.join(frames_path, f))
+    for f in range(1,numImg+1):
+        number = '{0:04d}'.format(f)
+        filename = os.path.join(frames_path, "frames" + number + ".jpg")       
+        img = dlib.load_rgb_image(filename)
         dets = cnn_face_detector(img, 1)
         sortedDets = sorted(dets, key=lambda a: a.confidence, reverse=True)
         if(len(dets) == 0):
