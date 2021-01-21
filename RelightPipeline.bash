a579660083e8e446ea0ce47a49ddfd5914ceca85
APikielny <apikielny@gmail.com>
2021-01-21 12:08:43 -0500

    starting relight bash script

---
+++ RelightPipeline.bash
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+#flow:
+
+# iterate through ids
+# for each id:
+#   create relit fakes (how many? different lighting conditions or angles?)
+# then run accuracy experiment
+
+cd '~/social-video-verification-v2/image-relighting'
+
+python live_lighting_transfer.py --light_text /home/socialvv/social-video-verification-v2/image-relighting/data/test/light/rotate_light_04.txt --input_path "/home/socialvv/Dataset/ID1/camera3.MP4" --output_path "/media/eleanor/New-Volume/socialvv/social-video-verification-v2/image-relighting-output/ID1/cam3.avi"
