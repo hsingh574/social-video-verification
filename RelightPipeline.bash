7b6d5eb9bb21dff801437ead22024797486fc578
APikielny <apikielny@gmail.com>
2021-01-21 16:18:01 -0500

    relight bash script

--- RelightPipeline.bash
+++ RelightPipeline.bash
@@ -13,15 +13,23 @@ IMAGE_RELIGHT_LIGHTING_DIR=${2:-"/home/socialvv/social-video-verification-v2/ima
 
 IMAGE_RELIGHT_OUTPUT=${3:-"/media/eleanor/New-Volume/socialvv/social-video-verification-v2/image-relighting-output"}
 
-echo ${IMAGE_RELIGHT_BASE_DIR}
+# echo ${IMAGE_RELIGHT_BASE_DIR}
 
 # cd ${IMAGE_RELIGHT_BASE_DIR} #TODO: why isn't this working?
 
-cd ..
+function relight_id {
 
-source lighting-env/bin/activate
+    echo 'relighting id'${1}
 
-cd image-relighting
+    cd ..
 
-echo 'Creating deep fake'
-"python" "live_lighting_transfer.py" "--light_text" "${IMAGE_RELIGHT_LIGHTING_DIR}/rotate_light_04.txt" "--input_path" "/home/socialvv/Dataset/ID1/camera3.MP4" "--output_path" "/${IMAGE_RELIGHT_OUTPUT}/ID1/bashScriptTest.avi"
+    source lighting-env/bin/activate
+
+    cd image-relighting
+
+    echo 'Creating deep fake'
+    "python" "live_lighting_transfer.py" "--light_text" "${IMAGE_RELIGHT_LIGHTING_DIR}/rotate_light_04.txt" "--input_path" "/home/socialvv/Dataset/ID1/camera3.MP4" "--output_path" "/${IMAGE_RELIGHT_OUTPUT}/ID1/bashScriptTest.avi"
+
+}
+
+relight_id 3
