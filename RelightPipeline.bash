3724bb3923172779db47185b98942276d3635ec2
APikielny <apikielny@gmail.com>
2021-01-21 15:49:17 -0500

    relight bash script

--- RelightPipeline.bash
+++ RelightPipeline.bash
@@ -7,11 +7,13 @@
 #   create relit fakes (how many? different lighting conditions or angles?)
 # then run accuracy experiment
 
-IMAGE_RELIGHT_DIR=${1:-"~/social-video-verification-v2/image-relighting"}
+IMAGE_RELIGHT_BASE_DIR=${1:-"~/social-video-verification-v2/image-relighting"}
 
-echo ${IMAGE_RELIGHT_DIR}
+IMAGE_RELIGHT_LIGHTING_DIR=${2:-"/home/socialvv/social-video-verification-v2/image-relighting/data/test/light"}
 
-# cd ${IMAGE_RELIGHT_DIR} #TODO: why isn't this working?
+echo ${IMAGE_RELIGHT_BASE_DIR}
+
+# cd ${IMAGE_RELIGHT_BASE_DIR} #TODO: why isn't this working?
 
 cd ..
 
@@ -19,4 +21,4 @@ source lighting-env/bin/activate
 
 cd image-relighting
 
-"python" "live_lighting_transfer.py" "--light_text" "/home/socialvv/social-video-verification-v2/image-relighting/data/test/light/rotate_light_04.txt" "--input_path" "/home/socialvv/Dataset/ID1/camera3.MP4" "--output_path" "/media/eleanor/New-Volume/socialvv/social-video-verification-v2/image-relighting-output/ID1/bashScriptTest.avi"
+"python" "live_lighting_transfer.py" "--light_text" "${IMAGE_RELIGHT_LIGHTING_DIR}/rotate_light_04.txt" "--input_path" "/home/socialvv/Dataset/ID1/camera3.MP4" "--output_path" "/media/eleanor/New-Volume/socialvv/social-video-verification-v2/image-relighting-output/ID1/bashScriptTest.avi"
