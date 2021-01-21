131233f395be346ba20d230c2b1bfe5b282ab81d
APikielny <apikielny@gmail.com>
2021-01-21 12:19:59 -0500

    relight bash script

--- RelightPipeline.bash
+++ RelightPipeline.bash
@@ -11,6 +11,7 @@ IMAGE_RELIGHT_DIR=${1:-"~/social-video-verification-v2/image-relighting"}
 
 echo ${IMAGE_RELIGHT_DIR}
 
-echo 'in relight dir'
+cd ${IMAGE_RELIGHT_DIR}
+
 
 #"python" "${IMAGE_RELIGHT_DIR}/live_lighting_transfer.py" "--light_text /home/socialvv/social-video-verification-v2/image-relighting/data/test/light/rotate_light_04.txt" "--input_path "/home/socialvv/Dataset/ID1/camera3.MP4"" "--output_path "/media/eleanor/New-Volume/socialvv/social-video-verification-v2/image-relighting-output/ID1/cam3.avi""
