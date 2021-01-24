eeff4832c6fe04d32909ad0e915848be62eb31ed
APikielny <apikielny@gmail.com>
2021-01-24 15:28:43 -0500

    add light analysis

--- RelightPipeline.bash
+++ RelightPipeline.bash
@@ -19,6 +19,10 @@ IMAGE_RELIGHT_OUTPUT=${3:-"/media/eleanor/New-Volume/socialvv/social-video-verif
 
 function relight_id {
 
+    ### to be changed:
+    ROTATE_LIGHT=${4:-"04"}
+    CAMERA_NUM=${5:-"3"}
+
     cd ..
 
     source lighting-env/bin/activate
@@ -28,7 +32,10 @@ function relight_id {
     mkdir -p "/${IMAGE_RELIGHT_OUTPUT}/ID${1}"
 
     echo 'Creating deep fake for id '${1}
-    "python" "live_lighting_transfer.py" "--light_text" "${IMAGE_RELIGHT_LIGHTING_DIR}/rotate_light_04.txt" "--input_path" "/home/socialvv/Dataset/ID${1}/camera3.MP4" "--output_path" "/${IMAGE_RELIGHT_OUTPUT}/ID${1}/bashScriptTest.avi"
+    "python" "live_lighting_transfer.py" "--light_text" "${IMAGE_RELIGHT_LIGHTING_DIR}/rotate_light_${ROTATE_LIGHT}.txt" "--input_path" "/home/socialvv/Dataset/ID${1}/camera${CAMERA_NUM}.MP4" "--output_path" "/${IMAGE_RELIGHT_OUTPUT}/ID${1}/light${ROTATE_LIGHT}_camera${CAMERA_NUM}.avi"
+
+    echo 'Getting landmarks for id '${1}
+    "python" "analyze_lighting_multiple.py" "--videos_path" "/home/socialvv/Dataset/ID${1}/" "--frames" "300" "--mat_path" "/home/socialvv/social-video-verification-v2/social-video-verification/Experiments/DataSHCoords/fake${CAMERA_NUM}-ID${1}.mat" "--fake_path /home/socialvv/socialvv/social-video-verification-v2/image-relighting-output/ID${1}/light${ROTATE_LIGHT}_camera${CAMERA_NUM}.avi"
 
 }
 
