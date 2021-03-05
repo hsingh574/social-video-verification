9cff50a6db9c88b405b0ca5a73cae580cef18447
APikielny <apikielny@gmail.com>
2021-03-04 21:05:46 -0500

    lower thresh

--- Experiments/RelightDetection/make_plots_v2.py
+++ Experiments/RelightDetection/make_plots_v2.py
@@ -289,7 +289,8 @@ def main():
     threshold_idx = 0
     
     if args.rocOn:
-        plot_ROC(ids, threshes, window_size, args.results_dir, args.save_dir)
+        # plot_ROC(ids, threshes, window_size, args.results_dir, args.save_dir)
+    plot_ROC(ids, [1.3], window_size, args.results_dir, args.save_dir)
         
     if args.accOn:
         plot_acc(ids, window_sizes, threshold, threshold_idx, args.results_dir, args.save_dir)
