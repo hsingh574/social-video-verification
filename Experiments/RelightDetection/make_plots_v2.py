3d300565db4560fa462f837b3bc9e28537a51954
APikielny <apikielny@gmail.com>
2021-03-05 09:49:58 -0500

    back to pca

--- Experiments/RelightDetection/make_plots_v2.py
+++ Experiments/RelightDetection/make_plots_v2.py
@@ -292,8 +292,8 @@ def main():
     
     
     window_size = 50
-    threshold = 0.5
-    threshold_idx = 0#TODO fix
+    threshold = 1.5
+    threshold_idx = 5#TODO fix
     
     if args.rocOn:
         plot_ROC(ids, threshes, window_size, args.results_dir, args.save_dir)
