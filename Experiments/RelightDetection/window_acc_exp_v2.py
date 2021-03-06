5f806d60e9888465e1847f9f1b7d8050b6c2b1ba
APikielny <apikielny@gmail.com>
2021-03-05 20:28:34 -0500

    where does the data break down

--- Experiments/RelightDetection/window_acc_exp_v2.py
+++ Experiments/RelightDetection/window_acc_exp_v2.py
@@ -195,6 +195,10 @@ def noPCA(cams, fake0, fake1, fake2, start, end, num_pcs, thresh):
     fake1Out = np.linalg.norm(fake1[start:end, :], axis = 1)
     fake2Out = np.linalg.norm(fake2[start:end, :], axis = 1)
 
+    print("cams diff", camsOut[0] - camsOut[1])
+    print("fake0 vs. cam diff", camsOut[0] - fake0Out[1])
+    print("fake1 vs. cam diff", camsOut[0] - fake0Out[1])
+
     # print("weighed dims", fake2Out)
 
     # print("PCA dims: ", fake0OutPCA.shape)
@@ -387,7 +391,7 @@ def gen_results(i, fake_cams, num_cams, zero_start, data_dir,
                 if end > fullLen-1:
                     continue
                 
-                numFakes0, numFakes1, numFakes2, numFakes3, c1, c2, c3 = onlyPCA(cams, fake0, fake1, fake2, start, end, num_pcs, t)
+                numFakes0, numFakes1, numFakes2, numFakes3, c1, c2, c3 = noPCA(cams, fake0, fake1, fake2, start, end, num_pcs, t)
                 # print("numFake0", numFakes0)
                 # print("numFake1", numFakes1)
                 # print("numFake2", numFakes2)
