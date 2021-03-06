6307990f4349b9024b5e957be33625456672d1de
APikielny <apikielny@gmail.com>
2021-03-05 21:57:07 -0500

    where does the data break down

--- Experiments/RelightDetection/window_acc_exp_v2.py
+++ Experiments/RelightDetection/window_acc_exp_v2.py
@@ -437,14 +437,14 @@ def gen_results(i, fake_cams, num_cams, zero_start, data_dir,
     fake0, fake1, fake2 = split_procedure(data0, data1, data2, real_cam0, 
                                           real_cam1, real_cam2, alternative, fullLen, intervalWin)
     
-    plt.title("cams plotted in gen results, only SH coord 1, red is fake")
-    plt.plot(cams[0][:,1], 'tab:blue')
-    plt.plot(cams[1][:,1], 'tab:orange')
-    plt.plot(cams[2][:,1], 'tab:green')
-    plt.plot(cams[3][:,1], 'tab:purple')
-    plt.plot(cams[4][:,1], 'tab:brown')
-    plt.plot(cams[5][:,1], 'tab:pink')
-    plt.plot(fake0[:,1], 'tab:red')
+    plt.title("cams plotted in gen results, only SH coord 2, red is fake")
+    plt.plot(cams[0][:,2], 'tab:blue')
+    plt.plot(cams[1][:,2], 'tab:orange')
+    plt.plot(cams[2][:,2], 'tab:green')
+    plt.plot(cams[3][:,2], 'tab:purple')
+    plt.plot(cams[4][:,2], 'tab:brown')
+    plt.plot(cams[5][:,2], 'tab:pink')
+    plt.plot(fake0[:,2], 'tab:red')
     plt.show()
     
     
