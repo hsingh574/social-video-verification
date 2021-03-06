6f9fb84888104f7a0b3f902fff13d3706d9ac1d2
APikielny <apikielny@gmail.com>
2021-03-05 20:38:06 -0500

    where does the data break down

--- Experiments/RelightDetection/window_acc_exp_v2.py
+++ Experiments/RelightDetection/window_acc_exp_v2.py
@@ -396,6 +396,7 @@ def gen_results(i, fake_cams, num_cams, zero_start, data_dir,
 
                 print("cams diff pre norm", np.mean(cams[0] - cams[1]))
                 print("cam vs fake0 diff pre norm", np.mean(cams[0] - fake0))
+                print("cam vs fake0 diff pre norm", np.mean(cams[1] - fake1))
 
                 numFakes0, numFakes1, numFakes2, numFakes3, c1, c2, c3 = noPCA(cams, fake0, fake1, fake2, start, end, num_pcs, t)
                 # print("numFake0", numFakes0)
