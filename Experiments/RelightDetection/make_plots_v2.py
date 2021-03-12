5eaf60f30955546c3bb2474396f9823916259ad5
APikielny <apikielny@gmail.com>
2021-03-12 12:55:17 -0500

    mean sh coords

--- Experiments/RelightDetection/make_plots_v2.py
+++ Experiments/RelightDetection/make_plots_v2.py
@@ -32,10 +32,10 @@ def parse_args():
                     help='Directory to save results plots')
     
     
-    # parser.add_argument("--thresholds", nargs="+", default=[1.3, 1.5, 1.7, 1.9, 2.1])
+    parser.add_argument("--thresholds", nargs="+", default=[1.3, 1.5, 1.7, 1.9, 2.1])
     # parser.add_argument("--thresholds", nargs="+", default=[0.5, 0.7, 0.9, 1.1, 1.3, 1.5])
     # parser.add_argument("--thresholds", nargs="+", default=[2.1, 2.3, 2.5, 2.7, 2.9])
-    parser.add_argument("--thresholds", nargs="+", default=[0.1, 0.5, 1.0, 1.5, 2.0, 5.0])
+    # parser.add_argument("--thresholds", nargs="+", default=[0.1, 0.5, 1.0, 1.5, 2.0, 5.0])
 
 
     # parser.add_argument("--thresholds", nargs="+", default=[7.0, 7.2, 7.4, 7.6, 7.8])
@@ -294,8 +294,8 @@ def main():
     
     
     window_size = 200
-    threshold = 0.5
-    threshold_idx = 1#TODO fix
+    threshold = 1.3
+    threshold_idx = 0#TODO fix
     
     if args.rocOn:
         plot_ROC(ids, threshes, window_size, args.results_dir, args.save_dir)
