2b5d1ce81fd12825007c53d1c8a29bee35e31c2d
APikielny <apikielny@gmail.com>
2021-03-15 11:57:40 -0400

    new lighting condition

--- Experiments/RelightDetection/window_acc_exp_v2.py
+++ Experiments/RelightDetection/window_acc_exp_v2.py
@@ -605,7 +605,9 @@ def main():
             continue
         
     exclude_list  = [17]
-    ids = [i for i in ids if i not in exclude_list] 
+    # ids = [i for i in ids if i not in exclude_list] 
+    ids = [1] 
+
     if not os.path.exists(args.save_dir):
         os.makedirs(args.save_dir)
     
