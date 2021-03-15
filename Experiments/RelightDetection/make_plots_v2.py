0f2303a1bfe10366536c56d1331e2cec2558a65e
APikielny <apikielny@gmail.com>
2021-03-15 15:40:48 -0400

    l2 across

--- Experiments/RelightDetection/make_plots_v2.py
+++ Experiments/RelightDetection/make_plots_v2.py
@@ -36,7 +36,9 @@ def parse_args():
     # parser.add_argument("--thresholds", nargs="+", default=[1.00, 1.3, 1.5, 1.7, 1.9, 2.1])
 
     # parser.add_argument("--thresholds", nargs="+", default=[0.5, 0.7, 0.9, 1.1, 1.3, 1.5])
-    parser.add_argument("--thresholds", nargs="+", default=[2.1, 2.3, 2.5, 2.7, 2.9])
+    parser.add_argument("--thresholds", nargs="+", default=[1.1])
+
+    # parser.add_argument("--thresholds", nargs="+", default=[2.1, 2.3, 2.5, 2.7, 2.9])
     # parser.add_argument("--thresholds", nargs="+", default=[0.1, 0.5, 1.0, 1.5, 2.0, 5.0])
 
 
@@ -296,8 +298,8 @@ def main():
     
     
     window_size = 200
-    threshold = 2.9
-    threshold_idx = 4#TODO fix
+    threshold = 1.1
+    threshold_idx = 0#TODO fix
     
     if args.rocOn:
         plot_ROC(ids, threshes, window_size, args.results_dir, args.save_dir)
