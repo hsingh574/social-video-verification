359389564fef7f249288eb16aa708b8ae22d95ae
APikielny <apikielny@gmail.com>
2021-03-15 18:42:54 -0400

    new dataset

--- RelightPipeline.bash
+++ RelightPipeline.bash
@@ -21,7 +21,7 @@ function relight_id { #params: id, camera to relight
 
     ### to be changed/randomized:
     # ROTATE_LIGHT=$(( RANDOM % 7 )) #random number (0 to 6)
-    ROTATE_LIGHT=0
+    ROTATE_LIGHT=4
     CAMERA_NUM=${2}
 
     cd ..
@@ -32,8 +32,8 @@ function relight_id { #params: id, camera to relight
 
     mkdir -p "/${IMAGE_RELIGHT_OUTPUT}/ID${1}"
 
-    # echo 'Creating deep fake for id '${1} 'and camera' ${CAMERA_NUM}
-    # "python" "live_lighting_transfer.py" "--light_text" "${IMAGE_RELIGHT_LIGHTING_DIR}/rotate_light_0${ROTATE_LIGHT}.txt" "--input_path" "/home/socialvv/Dataset/EleanorTrain/ID${1}/camera${CAMERA_NUM}.MP4" "--output_path" "/${IMAGE_RELIGHT_OUTPUT}/ID${1}/light${ROTATE_LIGHT}_camera${CAMERA_NUM}_id${1}.avi"
+    echo 'Creating deep fake for id '${1} 'and camera' ${CAMERA_NUM}
+    "python" "live_lighting_transfer.py" "--light_text" "${IMAGE_RELIGHT_LIGHTING_DIR}/rotate_light_0${ROTATE_LIGHT}.txt" "--input_path" "/home/socialvv/Dataset/EleanorTrain/ID${1}/camera${CAMERA_NUM}.MP4" "--output_path" "/${IMAGE_RELIGHT_OUTPUT}/ID${1}/light${ROTATE_LIGHT}_camera${CAMERA_NUM}_id${1}.avi"
 
     echo 'Getting sh coords for id '${1}
     "python" "analyze_lighting_multiple.py" "--videos_path" "/home/socialvv/Dataset/EleanorTrain/ID${1}/" "--frames" "1000" "--mat_path" "/home/socialvv/social-video-verification-v2/social-video-verification/Experiments/DataSHCoords/fake${CAMERA_NUM}-ID${1}.mat" "--fake_path" "/home/socialvv/socialvv/social-video-verification-v2/image-relighting-output/ID${1}/light${ROTATE_LIGHT}_camera${CAMERA_NUM}_id${1}.avi"
