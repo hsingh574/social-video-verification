bae90604052156b7474bbdbe9621113073581f69
APikielny <apikielny@gmail.com>
2021-03-15 14:59:19 -0400

    l2 across

--- Experiments/RelightDetection/make_plots_v2.py
+++ Experiments/RelightDetection/make_plots_v2.py
@@ -33,10 +33,10 @@ def parse_args():
     
     
     # parser.add_argument("--thresholds", nargs="+", default=[1.3, 1.5, 1.7, 1.9, 2.1])
-    parser.add_argument("--thresholds", nargs="+", default=[1.00, 1.3, 1.5, 1.7, 1.9, 2.1])
+    # parser.add_argument("--thresholds", nargs="+", default=[1.00, 1.3, 1.5, 1.7, 1.9, 2.1])
 
     # parser.add_argument("--thresholds", nargs="+", default=[0.5, 0.7, 0.9, 1.1, 1.3, 1.5])
-    # parser.add_argument("--thresholds", nargs="+", default=[2.1, 2.3, 2.5, 2.7, 2.9])
+    parser.add_argument("--thresholds", nargs="+", default=[2.1, 2.3, 2.5, 2.7, 2.9])
     # parser.add_argument("--thresholds", nargs="+", default=[0.1, 0.5, 1.0, 1.5, 2.0, 5.0])
 
 
