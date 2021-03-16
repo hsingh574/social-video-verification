d1ec504ac5786bd403a9d918accd01947bba969a
APikielny <apikielny@gmail.com>
2021-03-16 15:31:44 -0400

    new dataset testing

--- Experiments/RelightDetection/window_acc_exp_v2.py
+++ Experiments/RelightDetection/window_acc_exp_v2.py
@@ -374,7 +374,7 @@ def parse_args():
     parser.add_argument("--num-cams", type=int, default=6)
     # parser.add_argument("--thresholds", nargs="+", default=[0.5, 0.7, 0.9, 1.1, 1.3, 1.5])
     # parser.add_argument("--thresholds", nargs="+", default=[1.3, 1.5, 1.7, 1.9, 2.1])
-    parser.add_argument("--thresholds", nargs="+", default=[1.1])
+    parser.add_argument("--thresholds", nargs="+", default=[1.3])
 
     # parser.add_argument("--thresholds", nargs="+", default=[2.1, 2.3, 2.5, 2.7, 2.9])
     # parser.add_argument("--thresholds", nargs="+", default=[7.0, 7.2, 7.4, 7.6, 7.8])
