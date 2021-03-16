b73253590e753ed31df71b33ccfcb5e545bc0d23
APikielny <apikielny@gmail.com>
2021-03-16 15:39:39 -0400

    new dataset testing

--- Experiments/RelightDetection/make_plots_v2.py
+++ Experiments/RelightDetection/make_plots_v2.py
@@ -301,8 +301,8 @@ def main():
     
     
     window_size = 300
-    threshold = 1.3
-    threshold_idx = 1#TODO fix
+    threshold = 1.5
+    threshold_idx = 2#TODO fix
     
     if args.rocOn:
         plot_ROC(ids, threshes, window_size, args.results_dir, args.save_dir)
