a387c22b6f9d8527e50f0fba2c70fae293d9fa89
Alex Meyerowitz <alexandermeyerowitz@yahoo.com>
2021-03-17 23:07:29 -0400

    Updated ROC curve for comparison plots

--- Experiments/StandardDetectionV2/make_comparison_plots_for_paper.py
+++ Experiments/StandardDetectionV2/make_comparison_plots_for_paper.py
@@ -8,28 +8,29 @@ import argparse
 import os
 import numpy as np
 from scipy.io import loadmat
+from sklearn.metrics import roc_curve
 import re
 
 def parse_args():
     parser = argparse.ArgumentParser(description='DeepFake Detection Experiment')
     
-    parser.add_argument('--data-dir', type=str, default='data_v2',
+    parser.add_argument('--data-dir', type=str, default='Data',
                     help='Directory where data has been saved')
     
-    parser.add_argument('--results-dir', type=str, default='results_v2',
+    parser.add_argument('--results-dir', type=str, default='Adam method Results (14 IDs)',
                     help='Directory where results have been saved')
 
-    parser.add_argument('--results-dir-base1', type=str, default='results_v2',
+    parser.add_argument('--results-dir-base1', type=str, default='Mean feature method Results (14 IDs)',
                     help='Directory where baseline 1 results have been saved')
 
-    parser.add_argument('--results-dir-base2', type=str, default='results_v2',
+    parser.add_argument('--results-dir-base2', type=str, default='PCA method Results (14 IDs)',
                     help='Directory where baseline 2 results have been saved')
 
-    parser.add_argument('--save-dir', type=str, default='plots_v2',
+    parser.add_argument('--save-dir', type=str, default='Plots',
                     help='Directory to save results plots')
     
     
-    parser.add_argument("--thresholds", nargs="+", default=[1.2, 1.3, 1.5, 1.7, 1.9, 2.1, 2.5])
+    parser.add_argument("--thresholds", nargs="+", default=[1.2, 1.3, 1.5, 1.7, 1.9])
     parser.add_argument("--window-sizes", nargs="+", default=[10,20,30,40,50,60])
     
     
@@ -76,72 +77,59 @@ def plot_acc(ids, window_sizes, threshold, threshold_idx, results_dir, save_dir)
 
     return meanRes, stdRes    
  
-def plot_ROC(ids, threshes, window_size, results_dir, save_dir):
-    
-    threshNum = len(threshes)
-    numPeople = len(ids)
-    skipID = False
-    tpResults = np.zeros((threshNum,3,numPeople))
-    fpResults = np.zeros((threshNum,3,numPeople))
-    fpZeroFake = np.zeros((threshNum,1,numPeople))
-    
+def plot_ROC_v2(ids, threshes, window_size, results_dir, save_dir):
+    y_true_0 = None
+    y_score_0 = None
+
+    y_true_1 = None
+    y_score_1 = None
+
+    y_true_2 = None
+    y_score_2 = None
+
+    y_true_3 = None
+    y_score_3 = None
     for i,ID in enumerate(ids):
-        if skipID:
-            continue
-        for j, t in enumerate(threshes):
+        for j, threshold in enumerate(threshes):
             try:
                 results = loadmat(os.path.join(results_dir, 'ID{}'.format(ID),
-                                           'thresh_{}'.format(j), 
-                                           "window_{}.mat".format(window_size)))
-                skipID = False
+                                            'thresh_{}'.format(j), 
+                                            "p_window_{}.mat".format(window_size)))
             except FileNotFoundError:
-                skipID = True
-                break
-            
-            tpResults[j,0,i] = (np.sum(results['acc1'][0,:]) / (np.sum(results['acc1'][0,:]) + 
-                                                                 np.sum(results['acc1'][3,:])))
-            
-            tpResults[j,1,i] = (np.sum(results['acc2'][0,:]) / (np.sum(results['acc2'][0,:]) + 
-                                                                 np.sum(results['acc2'][3,:])))
-            
-            tpResults[j,2,i] = (np.sum(results['acc3'][0,:]) / (np.sum(results['acc3'][0,:]) + 
-                                                                 np.sum(results['acc3'][3,:])))
-            
-            fpResults[j,0,i] = (np.sum(results['acc1'][2,:]) / (np.sum(results['acc1'][2,:]) + 
-                                                                 np.sum(results['acc1'][1,:])))
-            
-            fpResults[j,1,i] = (np.sum(results['acc2'][2,:]) / (np.sum(results['acc2'][2,:]) + 
-                                                                 np.sum(results['acc2'][1,:])))
-            
-            fpResults[j,2,i] = (np.sum(results['acc3'][2,:]) / (np.sum(results['acc3'][2,:]) + 
-                                                                 np.sum(results['acc3'][1,:])))
-            
-            if (np.sum(results['acc0'][2,:]) + np.sum(results['acc0'][1,:])==0):
-                fpZeroFake[j,0,i] = 0
+                print("not found")
+                continue
+
+            if i == 0 and j == 0:
+                y_score_0 = results['p0'][0, :]
+                y_true_0 = results['p0'][1, :]
+
+                y_score_1 = results['p1'][0, :]
+                y_true_1 = results['p1'][1, :]
+
+                y_score_2 = results['p2'][0, :]
+                y_true_2 = results['p2'][1, :]
+
+                y_score_3 = results['p3'][0, :]
+                y_true_3 = results['p3'][1, :]
             else:
-                fpZeroFake[j,0,i] = (np.sum(results['acc0'][2,:]) / (np.sum(results['acc0'][2,:]) + 
-                                                                 np.sum(results['acc0'][1,:])))
-    
-    meanTP = np.mean(tpResults,axis = 2)
-    meanFP = np.mean(fpResults,axis = 2)
-    stdTP = np.std(tpResults,axis = 2, ddof=1)
-    stdFP = np.std(fpResults, axis = 2, ddof=1)
+                y_score_0 = np.hstack([y_score_0, results['p0'][0, :]])
+                y_true_0 = np.hstack([y_true_0, results['p0'][1, :]])
 
-    #zeroFakeMean = np.mean(fpZeroFake,axis = 2)
-    #zeroFakeStd = np.std(fpZeroFake,axis = 2)
+                y_score_1 = np.hstack([y_score_1, results['p1'][0, :]])
+                y_true_1 = np.hstack([y_true_1, results['p1'][1, :]])
 
-    #reformat & order by recall values
-    #sort rows by first column
-    
-    oneFake = np.column_stack([meanFP[:,0], meanTP[:,0]])
-    twoFake = np.column_stack([meanFP[:,1], meanTP[:,1]])
-    thrFake = np.column_stack([meanFP[:,2], meanTP[:,2]])
-    
-    oneFake = oneFake[np.argsort(oneFake[:, 0])]
-    twoFake = twoFake[np.argsort(twoFake[:, 0])]
-    thrFake = thrFake[np.argsort(thrFake[:, 0])]
+                y_score_2 = np.hstack([y_score_2, results['p2'][0, :]])
+                y_true_2 = np.hstack([y_true_2, results['p2'][1, :]])
 
-    return oneFake,twoFake,thrFake,stdTP,stdFP
+                y_score_3 = np.hstack([y_score_3, results['p3'][0, :]])
+                y_true_3 = np.hstack([y_true_3, results['p3'][1, :]])
+        
+    #fpr0, tpr0, _  = roc_curve(y_true_0, y_score_0)
+    fpr1, tpr1, _  = roc_curve(y_true_1, y_score_1)
+    fpr2, tpr2, _  = roc_curve(y_true_2, y_score_2)
+    fpr3, tpr3, _  = roc_curve(y_true_3, y_score_3)
+
+    return fpr1, tpr1, fpr2, tpr2, fpr3, tpr3
 
 def plot_vs_baselines_acc(ids, window_sizes, threshold, threshold_idx, results, save_dir):
 
@@ -152,7 +140,6 @@ def plot_vs_baselines_acc(ids, window_sizes, threshold, threshold_idx, results, 
  
     # For each method, plot 0-3 fakes
     for method in results:
-        print("Method: ")
         curMeans,curStds = plot_acc(ids, window_sizes, threshold, threshold_idx, results[method], save_dir)
 
         ax0.errorbar(window_sizes, curMeans[0,:], yerr = curStds[0,:], label = method, elinewidth=0.5, capsize=1)
@@ -205,30 +192,30 @@ def plot_vs_baselines_ROC(ids, threshes, window_size, results, save_dir):
 
     # For each method, plot 0-3 fakes
     for method in results:
-        oneFake,twoFake,thrFake,stdTP,stdFP = plot_ROC(ids, threshes, window_size, results[method], save_dir)
+        fpr1, tpr1, fpr2, tpr2, fpr3, tpr3 = plot_ROC_v2(ids, threshes, window_size, results[method], save_dir)
         
-        ax0.errorbar(oneFake[:,0], oneFake[:,1], yerr = stdTP[:,0], xerr = stdFP[:,0], label = method)
-        ax1.errorbar(twoFake[:,0], twoFake[:,1], yerr = stdTP[:,1], xerr = stdFP[:,1], label = method)
-        ax2.errorbar(thrFake[:,0], thrFake[:,1], yerr = stdTP[:,2], xerr = stdFP[:,2], label = method)
+        ax0.plot(fpr1, tpr1, label = method)
+        ax1.plot(fpr2, tpr2, label = method)
+        ax2.plot(fpr3, tpr3, label = method)
 
 
     # Graphing settings
     ax0.set_xlim([0, 1])
-    ax0.set_ylim([0, 1])
+    ax0.set_ylim([0, 1.1])
     ax0.set_ylabel('True Positive Rate')
     ax0.set_xlabel('False Positive Rate')
     ax0.legend(loc = 'lower right')
     ax0.set_title('ROC Curve, {} Fakes, Window size = {}'.format(1, window_size))
 
     ax1.set_xlim([0, 1])
-    ax1.set_ylim([0, 1])
+    ax1.set_ylim([0, 1.1])
     ax1.set_ylabel('True Positive Rate')
     ax1.set_xlabel('False Positive Rate')
     ax1.legend(loc = 'lower right')
     ax1.set_title('ROC Curve, {} Fakes, Window size = {}'.format(2, window_size))
 
     ax2.set_xlim([0, 1])
-    ax2.set_ylim([0, 1])
+    ax2.set_ylim([0, 1.1])
     ax2.set_ylabel('True Positive Rate')
     ax2.set_xlabel('False Positive Rate')
     ax2.legend(loc = 'lower right')
@@ -263,8 +250,8 @@ def main():
     window_sizes = args.window_sizes
     
     window_size = 60
-    threshold = 2.1
-    threshold_idx = 5
+    threshold = 1.5
+    threshold_idx = 2
 
     # Change dictionary entry titles depending on your baselines/baseline order input
     results = {'Ours':args.results_dir, 'Baseline 1': args.results_dir_base1, 'Baseline 2': args.results_dir_base2}
