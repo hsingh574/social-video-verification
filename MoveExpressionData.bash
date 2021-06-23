31b0e71242142d8dbc6cfa5afdc13dff025a9abc
APikielny <apikielny@gmail.com>
2021-06-23 16:45:30 -0700

    add expression script

---
+++ MoveExpressionData.bash
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# iterate through ids
+# for each id:
+#   move videos
+
+
+EXPRESSION_OUTPUT=${3:-"/media/eleanor/New-Volume/expression-data"}
+
+function move_id { #params: id, camera to relight
+
+    echo 'Copying over reals for id ' ${1}
+    cams=(0 1 2 3 4 5 6)
+    for cam_num in ${cams[@]};
+    do
+        mkdir -p "/${EXPRESSION_OUTPUT}/ID${1}/real/cam${cam_num}/"
+        cp "/home/socialvv/Dataset/DeepForensicsReal/ID${1}/camera${cam_num}.mp4" "/${EXPRESSION_OUTPUT}/ID${1}/real/cam${cam_num}/camera${cam_num}.mp4"
+
+        mkdir -p "/${EXPRESSION_OUTPUT}/ID${1}/fake/cam${cam_num}/"
+        cp "/home/socialvv/Dataset/DeepForensicsFake/ID${1}/camera${cam_num}.mp4" "/${EXPRESSION_OUTPUT}/ID${1}/fake/cam${cam_num}/camera${cam_num}.mp4"
+
+    done
+
+
+}
+
+ids=(1020 1084 150 219 419 501 63 679 685 785 800 875 908 949)
+
+for id in ${ids[@]};
+do
+    move_id ${id}
+done
+
+#TODO copy mat files
